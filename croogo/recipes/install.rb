8e0479b6d244a109ee56e5942cca0a9f5976634d
Juan Basso <jrbasso@gmail.com>
2013-06-27 23:36:09 -0400

    Giving write permission on tmp folder

--- croogo/recipes/install.rb
+++ croogo/recipes/install.rb
@@ -4,6 +4,10 @@ node[:deploy].each do |application, deploy|
     next
   end
 
+  directory "#{deploy[:deploy_to]}/current/tmp" do
+	mode 0777
+  end
+
   execute 'make croogo' do
 	action :run
 	user deploy[:user]
