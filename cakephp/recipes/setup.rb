c66559f5e184482fc10573a70686ae73fd77aae6
Juan Basso <jrbasso@gmail.com>
2013-06-27 22:38:54 -0400

    Do not reinstall CakePHP on the setup

--- cakephp/recipes/setup.rb
+++ cakephp/recipes/setup.rb
@@ -7,12 +7,14 @@ node[:deploy].each do |application, deploy|
   execute 'setup CakePHP PEAR channel' do
 	action :run
 	command 'pear channel-discover pear.cakephp.org'
+	not_if 'pear list-channels | grep pear.cakephp.org'
 	user 'root'
   end
 
   execute 'install CakePHP PEAR package' do
 	action :run
 	command 'pear install cakephp/CakePHP'
+	not_if 'pear list -a | grep CakePHP'
 	user 'root'
   end
 end
