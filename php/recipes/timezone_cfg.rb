ee43411c1894cfbfa250ceb76e17cee3b959e3c9
Juan Basso <jrbasso@gmail.com>
2013-06-28 00:36:05 -0400

    Restart apache after change the php timezone

--- php/recipes/timezone_cfg.rb
+++ php/recipes/timezone_cfg.rb
@@ -1,4 +1,6 @@
 node[:deploy].each do |application, deploy|
+  include_recipe 'apache2::service'
+
   if deploy[:application_type] != 'php'
     Chef::Log.debug("Skipping php::timezone_cfg application #{application} as it is not an PHP app")
     next
@@ -8,5 +10,6 @@ node[:deploy].each do |application, deploy|
 	action :run
 	command "sed -i 's/;date\.timezone.*/date.timezone = UTC/g' `php -r 'echo php_ini_loaded_file();'`"
 	user 'root'
+	notifies :reload, resources(:service => "apache2"), :delayed
   end
 end
