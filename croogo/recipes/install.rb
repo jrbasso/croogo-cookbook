44f53bb0984530ba15f568fcffa26271c469e801
Juan Basso <jrbasso@gmail.com>
2013-06-27 22:57:18 -0400

    Submodules are updated by chef

--- croogo/recipes/install.rb
+++ croogo/recipes/install.rb
@@ -4,12 +4,6 @@ node[:deploy].each do |application, deploy|
     next
   end
 
-  execute 'install croogo submodules' do
-	action :run
-	user deploy[:user]
-	command "cd #{deploy[:deploy_to]} && git submodule update --init"
-  end
-
   execute 'make croogo' do
 	action :run
 	user deploy[:user]
