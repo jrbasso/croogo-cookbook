21985195ac287f11d71ab9e976cd2a97eb44a871
Juan Basso <jrbasso@gmail.com>
2013-06-29 20:17:05 -0400

    Added recipe for croogo database setup

---
+++ croogo/recipes/dbsetup.rb
@@ -0,0 +1,22 @@
+node[:deploy].each do |application, deploy|
+  execute "create mysql database" do
+    command "/usr/bin/mysql -u#{node[:croogo][:database][:username]} -p#{node[:croogo][:database][:password]} -e 'CREATE DATABASE IF NOT EXISTS #{node[:croogo][:database][:database]}'"
+    action :run
+  end
+
+  execute "create mysql tables" do
+    command "/usr/bin/mysql -u#{node[:croogo][:database][:username]} -p#{node[:croogo][:database][:password]} #{node[:croogo][:database][:database]} < #{deploy[:deploy_to]}/current/Config/Schema/sql/croogo.sql"
+    action :run
+  end
+
+  execute "import mysql data" do
+    command "/usr/bin/mysql -u#{node[:croogo][:database][:username]} -p#{node[:croogo][:database][:password]} #{node[:croogo][:database][:database]} < #{deploy[:deploy_to]}/current/Config/Schema/sql/croogo_data.sql"
+    action :run
+  end
+
+  execute "mark croogo as installed" do
+    command "/usr/bin/mysql -u#{node[:croogo][:database][:username]} -p#{node[:croogo][:database][:password]} #{node[:croogo][:database][:database]} -e 'INSERT IGNORE INTO `settings` (`id`, `key`, `value`, `title`, `description`, `input_type`, `editable`, `weight`, `params`) VALUES (0, \'Croogo.installed\', \'1\', \'\', \'\', \'\', 1, 1, \'\')'"
+    action :run
+  end
+
+end
