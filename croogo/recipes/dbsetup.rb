6df8192a1ee88e3f9985ac306c31fcfa265c010c
Juan Basso <jrbasso@gmail.com>
2013-07-23 22:00:57 -0400

    Using db for sessions

--- croogo/recipes/dbsetup.rb
+++ croogo/recipes/dbsetup.rb
@@ -14,6 +14,11 @@ node[:deploy].each do |application, deploy|
     action :run
   end
 
+  execute "create mysql session table" do
+    command "/usr/bin/mysql -u#{node[:croogo][:database][:username]} -p#{node[:croogo][:database][:password]} #{node[:croogo][:database][:database]} -e 'CREATE TABLE IF NOT EXISTS cake_sessions (id varchar(255) NOT NULL default \'\', data text, expires int(11) default NULL, PRIMARY KEY  (id));'"
+    action :run
+  end
+
   execute "import mysql data" do
     command "/usr/bin/mysql -u#{node[:croogo][:database][:username]} -p#{node[:croogo][:database][:password]} #{node[:croogo][:database][:database]} < #{deploy[:deploy_to]}/current/Config/Schema/sql/croogo_data.sql"
     not_if command "/usr/bin/mysql -u#{node[:croogo][:database][:username]} -p#{node[:croogo][:database][:password]} #{node[:croogo][:database][:database]} -e 'SELECT id FROM settings LIMIT 1'"
